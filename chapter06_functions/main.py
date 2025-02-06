ae1479018fd31f5a7ee0ce5882810546c80ff376
maybeage <suren0709@gmail.com>
2025-02-06 11:39:34 +0900

    feat: 20250206 functions / scope done, collections stared

--- chapter06_functions/main.py
+++ chapter06_functions/main.py
@@ -156,10 +156,76 @@ vending_machine(3000)
 음료수 = 3개, 잔돈 = 900원
 음료수 = 4개, 잔돈 = 200원
 '''
+# def vending_machine():
+#     pass                        # 이거 입력하면 함수 구현이 되어있지 않더라도 일단 오류가 발생하지 않음.
+#
+#
+# #함수 호출
+# vending_machine(3000)
 
 
+# 여기에 작성하고 함수화하겠습니다.
+
+# my_money = 3000
+# drink_price = 700
+# for 반복문 돌려서 실행 예와 같이 나오도록 하려면 어떻게 작성해야할까요? -> f-string
+# charge = 3000 - (700*음료수 개수)
+# my_money를 기준으로 음료수를 살 수 있는 경우의 수를 고려하면 0 ~ 4개까지 반복문이 5번 돌아가네요.
+# for i in range(my_money//drink_price + 1):         # range() 함수 내에 들어가있는 argument의 자료형은 int여야 합니다. -> 단순히 나눠서는 안되죠
+#     print(f"음료수 = {i}개, 잔돈 = {my_money-(drink_price*i)}")
+
+# return 타입 없으니까 print()로 마무리 지어놨습니다 / 매개변수 명이 통제돼있으므로 vending_machine(money)형태로 작성.
+# def vending_machine(money):
+#     for i in range(money // 700 + 1):
+#         print(f"음료수 = {i}개, 잔돈 = {money - (700 * i)}")
+#
+#
+# # 함수 호출
+# vending_machine(7500)
+
+'''
+예제 : 구구단 출력하기
+함수 정의 :
+함수 이름 : multiply
+매개변수 : 정수 n
+리턴값 : 없음
+
+함수 호출 :
+multiply(dan)
+
+실행 예
+몇 단을 출력하시겠습니까? >>> 3
+3 x 1 = 3
+...
+3 x 9 = 27
+
+ctrl + shift + f 를 사용해서 구구단 출력하는 코드 찾아와서 (혹은 직접 작성하셔도 무방), 함수화시켜보세요.
+'''
+# 함수 정의 -> call2 유형으로 작성했습니다.
+# def multiply(n):
+#     for i in range(1, 10, 1):  # range(시작값, 종료값, 증감값)
+#         print(f"{n} x {i} = {n * i}")
+
+# 함수 정의 -> call1 유형으로 작성할 때의 예시
+def multiply2():
+    dan = int(input("몇 단을 출력하시겠습니까? >>> "))
+    for i in range(1, 10, 1):
+        print(f"{dan} x {i} = {dan * i}")
+
+# print(dan)        # 현재 상황에서 오류 발생 -> 함수를 정의만 하는 것은 사용한 게 아니기 때문에 변수에 해당하지가 않는다.
+# dan = 3           # 그리고 structure를 확인한 결과 선언한 dan이 없음을 확인할 수 있음.
+# print(dan)
+i = 3
+
+# call2 유형을 호출
+# dan = int(input("몇 단을 출력하시겠습니까? >>> "))
+#
+# # 함수 호출
+# multiply(dan)
+
+# call1 유형을 호출
+# multiply2()
 
 
-
-
-
+# 연습이 필요하신 분은 leap_year를 함수화시켜보시는 것을 추천합니다.
+# chapter06내에 scope 패키지 생성하고, main.py 만드세요
